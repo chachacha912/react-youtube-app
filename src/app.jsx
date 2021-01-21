38b55cc8ec35ca3a727fdb18403a723fba27b4b1
chachacha912 <dkwkdnwk@naver.com>
2021-01-21 10:03:11 +0900

    Refactor youtube service(youtube class 추가 & 환경변수 설정)

--- src/app.jsx
+++ src/app.jsx
@@ -3,47 +3,24 @@ import './app.css';
 import SearchForm from './components/search_form/search_form';
 import VideoLists from './components/video_list/video_list';
 
-const App = () => {
+const App = ({ youtube }) => {
     const [isLoading, setLoading] = useState(true);
     const [videos, setVideos] = useState([]);
 
-    const handleSearch = useCallback((item) => {
-        // console.log(item);
+    const handleSearch = useCallback((query) => {
+        // console.log(query);
         setLoading(true);
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow',
-        };
-
-        // key=AAA 에 발급받은 키 입력하기
-        fetch(
-            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${item}&type=video&key=AAA`,
-            requestOptions
-        )
-            .then((response) => response.json())
-            .then((result) =>
-                result.items.map((item) => ({ ...item, id: item.id.videoId }))
-            )
-            .then((items) => setVideos(items))
-            .catch((error) => console.log('error', error))
+        youtube
+            .search(query) //
+            .then((videos) => setVideos(videos))
             .finally(setLoading(false));
     });
 
     useEffect(() => {
         // console.log('mounted');
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow',
-        };
-
-        // key=AAA 에 발급받은 키 입력하기
-        fetch(
-            'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AAA',
-            requestOptions
-        )
-            .then((response) => response.json())
-            .then((result) => setVideos(result.items))
-            .catch((error) => console.log('error', error))
+        youtube
+            .mostPopular() //
+            .then((videos) => setVideos(videos))
             .finally(setLoading(false));
     }, []);
 
